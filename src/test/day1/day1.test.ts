494bba7708534029c975cff486db0e11d5a5e1ac
Siiri Saarela <siiri.saarela@digital.cabinet-office.gov.uk>
2022-12-01 22:43:27 +0000

    advent-of-code: Day 1

---
+++ src/test/day1/day1.test.ts
@@ -0,0 +1,79 @@
+
+const {readFileSync, promises: fsPromises} = require('fs');
+
+async function asyncReadFile(filename: any) {
+  try {
+    const contents = await fsPromises.readFile(`${__dirname}/${filename}`, 'utf-8');
+    const arr = contents.split(/\r?\n/);
+    return arr;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+describe("Advent of code day 1", () => {
+  it("Part 1 - Get highest calories", async () => {
+    const array = await asyncReadFile('data.txt');
+
+    const testArray1 = ['1', '1', '1', '', '2', '2', '2', '', '3']
+    const testArray2 = ['1', '1', '1', '', '2', '2', '2', '', '5', '90']
+
+    function getHighestCalories(array :any) {
+      let highestCalories = 0
+      let currentCalories = 0
+
+      for (let i = 0; i < array.length; i++) {
+        const groupEnded = array[i] === ''
+        if (groupEnded) {
+            if (currentCalories > highestCalories) highestCalories = currentCalories
+            currentCalories = 0
+        } else {
+          currentCalories = currentCalories + +array[i]
+        }
+      }
+
+      if (highestCalories < currentCalories) highestCalories = currentCalories
+
+      return highestCalories
+    }
+    
+    expect(getHighestCalories(testArray1)).toEqual(6)
+    expect(getHighestCalories(testArray2)).toEqual(95)
+    expect(getHighestCalories(array)).toEqual(70720)
+
+  });
+
+  it("Part 2 - Get highest calories of top 3 reindeer", async () => {
+    const array = await asyncReadFile('data.txt');
+
+    const testArray1 = ['1', '1', '', '1', '1', '1', '', '1', '1', '1', '1', '', '1', '1', '1', '1', '1' ]
+    const testArray2 = ['50', '25', '25', '', '2', '2', '2', '', '10', '90', '', '50', '50']
+
+    function getHighestCalories(array :any) {
+      let currentCalories = 0
+      const calorieGroups: number[] = []
+
+      for (let i = 0; i < array.length; i++) {
+        const groupEnded = array[i] === ''
+        if (groupEnded) {
+            calorieGroups.push(currentCalories)
+            currentCalories = 0
+        } else {
+          currentCalories = currentCalories + +array[i]
+        }
+
+      }
+      calorieGroups.push(currentCalories)
+
+      const topThreeCalories = calorieGroups.sort((a, b) => b - a).slice(0,3)
+      const topThreeCaloriesSum = topThreeCalories.reduce((a, b) => a + b, 0)
+
+      return topThreeCaloriesSum
+    }
+    
+    expect(getHighestCalories(testArray1)).toEqual(12)
+    expect(getHighestCalories(testArray2)).toEqual(300)
+    expect(getHighestCalories(array)).toEqual(207148)
+
+  });
+});
