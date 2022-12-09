9b2ad187b594e55dcb956830fa2790041079ff69
Siiri Saarela <siiri.saarela@digital.cabinet-office.gov.uk>
2022-12-09 09:00:39 +0000

    advent-of-code: Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+ singleQuote: false,
+ printWidth: 120,
+ trailingComma: "none",
+ bracketSpacing: true,
+ htmlWhitespaceSensitivity: "ignore",
+ semi: false,
+ quoteProps: "preserve"
+}
